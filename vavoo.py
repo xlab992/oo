460149877bd23666b6816e7727d5ab632a9a2ddf
ciccioxm3 <cicciariello@proton.me>
2025-05-27 22:23:22 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -5,9 +5,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-PROXYMFP = os.getenv("PROXYMFP")
+PROXYMFP = os.getenv("PROXYMFP", "")
 NOMEGITHUB = os.getenv("NOMEGITHUB")
 NOMEREPO = os.getenv("NOMEREPO")
+PROXYMFPNOPSW = os.getenv("PROXYMFPNOPSW", "")
 
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
 OUTPUT_FILE = "channels_italy.m3u8"

6dde9d2dac34bf7a1c5242522da94ac373aeae97
ciccioxm3 <cicciariello@proton.me>
2025-06-08 10:16:21 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -317,7 +317,7 @@ def process_events():
                                     tvg_name = f"{time_only} {event_details} - {day_num}/{month_num}/{year_short}"
 
                                     file.write(f'#EXTINF:-1 tvg-id="{event_name} - {event_details.split(":", 1)[1].strip() if ":" in event_details else event_details}" tvg-name="{tvg_name}" tvg-logo="{LOGO}" group-title="{clean_sport_key}", {channel["channel_name"]}\n')
-                                    file.write(f"{PROXY}{DDMFP}{stream_url_dynamic}{DDMFP2}\n\n")
+                                    file.write(f"{PROXY}{MFPDD}{stream_url_dynamic}{MFPDD2}\n\n")
 
                                 processed_channels += 1
                                 filtered_channels += 1
